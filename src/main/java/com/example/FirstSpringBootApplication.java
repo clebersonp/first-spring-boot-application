f54883a3060d91a1d5c5127aa0cbe5257e7fd423
Cleberson <pauluci.cleberson@gmail.com>
2021-07-31 19:40:53 -0300

    Alterado endpoint para receber query param 'name' e retornar json de saudacao

--- src/main/java/com/example/FirstSpringBootApplication.java
+++ src/main/java/com/example/FirstSpringBootApplication.java
@@ -1,9 +1,14 @@
 package com.example;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Greeting;
 
 /**
  * 
@@ -14,16 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
  * Boot para realizar a auto configuração baseado nas dependências adicionadas
  * no projeto.
  * 
- * Maven version: 3.6.1 ou superior
- * JDK version: 11 ou superior
+ * Maven version: 3.6.1 ou superior JDK version: 11 ou superior
  * 
  * Na raiz do projeto:
  * 
- * Compilar e gerar um jar: JAVA_HOME=/usr/lib/jvm/jdk-11.0.10/ mvn spring-boot:run
- * Executar a aplicação via plugin maven: JAVA_HOME=/usr/lib/jvm/jdk-11.0.10/ mvn clean install
- * Executar a aplicação via java -jar: java -jar target/first-spring-boot-application-0.0.1-SNAPSHOT.jar
- * Abrir o endereço localhost:8080 no browser
- * Finalizar a aplicação: ctrl-c
+ * Compilar e gerar um jar: JAVA_HOME=/usr/lib/jvm/jdk-11.0.10/ mvn
+ * spring-boot:run Executar a aplicação via plugin maven:
+ * JAVA_HOME=/usr/lib/jvm/jdk-11.0.10/ mvn clean install Executar a aplicação
+ * via java -jar: java -jar
+ * target/first-spring-boot-application-0.0.1-SNAPSHOT.jar Abrir o endereço
+ * localhost:8080 no browser Finalizar a aplicação: ctrl-c
  * 
  * @author cleberson
  *
@@ -32,9 +37,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class FirstSpringBootApplication {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting getGreeting(@RequestParam(value = "name", defaultValue = "World") final String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
 	public static void main(String[] args) {
